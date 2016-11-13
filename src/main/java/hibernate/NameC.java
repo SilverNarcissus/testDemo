5ac0dd46debea75609f8ec12f10b8666ee9bf83c
Silver Narcissus <827011867@qq.com>
2016-11-13 18:23:48 +0800

    new

---
+++ src/main/java/hibernate/NameC.java
@@ -0,0 +1,16 @@
+package hibernate;
+
+/**
+ * Created by SilverNarcissus on 16/11/13.
+ */
+public class NameC {
+    private String name;
+
+    public NameC(String name){
+        this.name=name;
+    }
+    @Override
+    public String toString() {
+        return name;
+    }
+}
