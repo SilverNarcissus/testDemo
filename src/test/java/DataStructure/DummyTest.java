63db71c1eb67b637854358973571e81bef446fba
Silver Narcissus <827011867@qq.com>
2016-12-23 19:00:06 +0800

    for test

---
+++ src/test/java/DataStructure/DummyTest.java
@@ -0,0 +1,120 @@
+package DataStructure;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by SilverNarcissus on 2016/12/23.
+ */
+public class DummyTest {
+    @Test
+    public void test1() {
+        fail();
+    }
+
+    @Test
+    public void test2() {
+        fail();
+    }
+
+    @Test
+    public void test3() {
+        fail();
+    }
+
+    @Test
+    public void test4() {
+        fail();
+    }
+
+    @Test
+    public void test5() {
+        fail();
+    }
+
+    @Test
+    public void test6() {
+
+    }
+
+    @Test
+    public void test7() {
+
+    }
+
+    @Test
+    public void test8() {
+
+    }
+
+    @Test
+    public void test9() {
+
+    }
+
+    @Test
+    public void test10() {
+
+    }
+
+    @Test
+    public void test11() {
+
+    }
+
+    @Test
+    public void test12() {
+
+    }
+
+    @Test
+    public void test13() {
+
+    }
+
+    @Test
+    public void test14() {
+        fail();
+    }
+
+    @Test
+    public void test15() {
+
+    }
+
+    @Test
+    public void test16() {
+
+    }
+
+    @Test
+    public void test17() {
+
+    }
+
+    @Test
+    public void test18() {
+
+    }
+
+    @Test
+    public void test19() {
+        fail();
+    }
+
+    @Test
+    public void test20() {
+
+    }
+
+    @Test
+    public void test21() {
+        fail();
+    }
+
+    @Test
+    public void test22() {
+        fail();
+    }
+}
